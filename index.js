f1f9110814ac25503e7305a1586501f86a0677bc
kaneki33 <abdulaziz331998@gmail.com>
2018-07-24 13:13:41 +0300

    Add cool face API

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 //
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup'),
@@ -194,7 +198,13 @@ bot.on('text', (ctx) => {
         
     
 
-
+    express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .get('/cool', (req, res) => res.send(cool()))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 

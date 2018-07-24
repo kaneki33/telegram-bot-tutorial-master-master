924d4e7c8b57109f559a678001768ceb7bff9868
kaneki33 <abdulaziz331998@gmail.com>
2018-07-24 13:29:27 +0300

    Add cool face API

--- index.js
+++ index.js
@@ -178,13 +178,7 @@ bot.on('text', (ctx) => {
         
     
 
-    express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
-    .get('/cool', (req, res) => res.send(cool()))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+   
 
 
 

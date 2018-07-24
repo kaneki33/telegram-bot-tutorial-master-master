678ef2c80ac2749c12efbb8627d38b366432f151
kaneki33 <abdulaziz331998@gmail.com>
2018-07-24 13:22:29 +0300

    Add cool face API

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 'use strict';
 //
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
 const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup'),
@@ -174,23 +170,7 @@ bot.hears(['bye', 'Bye'],  (ctx) => {
     
     
 
-    const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
 
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM test_table');
-    res.render('pages/db', result);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-});
 bot.on('text', (ctx) => {
   ctx.telegram.sendMessage(ctx.message.chat.id, `GO TO HELL!!!!!`) })
 

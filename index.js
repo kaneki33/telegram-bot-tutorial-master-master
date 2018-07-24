855becea1ecf6a6bebfa6a8fc3a340f6fd9a6f63
kaneki33 <abdulaziz331998@gmail.com>
2018-07-24 13:29:53 +0300

    Add cool face API

--- index.js
+++ index.js
@@ -174,6 +174,27 @@ bot.hears(['bye', 'Bye'],  (ctx) => {
 bot.on('text', (ctx) => {
   ctx.telegram.sendMessage(ctx.message.chat.id, `GO TO HELL!!!!!`) })
 
+
+
+
+
+  const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
     bot.startPolling()
         
     

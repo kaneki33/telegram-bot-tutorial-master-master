be7bd9db416a2c5e37655ac7e199aadf986aa5c8
kaneki33 <abdulaziz331998@gmail.com>
2018-08-02 22:53:27 +0300

    test

--- index.js
+++ index.js
@@ -212,9 +212,10 @@ function exitHandler(exitCode) {
     storage.flush();
     process.exit(exitCode);
 }
-bot.telegram.setWebhook("https://serene-beyond-78420.herokuapp.com/" + token);
-
-bot.startWebhook('/'+token, null, process.env.PORT);
 
 process.on('SIGINT', exitHandler.bind(null, 0));
 process.on('uncaughtException', exitHandler.bind(null, 1));
+
+bot.telegram.setWebhook("https://serene-beyond-78420.herokuapp.com/" + token);
+
+bot.startWebhook('/'+token, null, process.env.PORT);

a643b00227de72a3ec2d6fae4d1fbb176f43a847
kaneki33 <abdulaziz331998@gmail.com>
2018-08-08 02:52:06 +0300

    test

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ var nick = fs.readFileSync(dirname + '/nicknames.json')
 
 bot.command('start', (ctx) => {
   return ctx.reply(`welcome \nPlease enter the nick-name you want to be called \n\n Notice: Its an irreversible action so please choose well`
-, addnick(ctx.message) 
+//, addnick(ctx.message) 
 
 )})
 

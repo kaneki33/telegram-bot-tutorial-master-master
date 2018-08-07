9574be0ceedf641e0be852ab5055b2fe97677b46
kaneki33 <abdulaziz331998@gmail.com>
2018-08-08 02:13:21 +0300

    test

--- index.js
+++ index.js
@@ -40,26 +40,52 @@ bot.command('start', (ctx) => {
 
 )})
 
-var standard_input = process.stdin;
+// Include prompt module.
+var prompt = require('prompt');
 
-// Set input character encoding.
-standard_input.setEncoding('utf-8');
-
-// Prompt user to input data in console.
-console.log("Please input text in command line.");
-
-// When user input data and click enter key.
-standard_input.on('data', function (data) {
-
-    // User input exit.
-    if(data === 'exit\n'){
-        // Program exit.
-        console.log("User input complete, program exit.");
-        process.exit();
-    }else
+// This json object is used to configure what data will be retrieved from command line.
+var prompt_attributes = [
     {
-        // Print user input in console.
-        console.log('User Input Data : ' + data);
+        // The fist input text is assigned to username variable.
+        name: 'username',
+        // The username must match below regular expression.
+        validator: /^[a-zA-Z\s\-]+$/,
+        // If username is not valid then prompt below message.
+        warning: 'Username is not valid, it can only contains letters, spaces, or dashes'
+    },
+    {
+        // The second input text is assigned to password variable.
+        name: 'password',
+        // Do not show password when user input.
+        hidden: true
+    },
+    {
+        // The third input text is assigned to email variable.
+        name: 'email',
+        // Display email address when user input.
+        hidden: false
+    }
+];
+
+// Start the prompt to read user input.
+prompt.start();
+
+// Prompt and get user input then display those data in console.
+prompt.get(prompt_attributes, function (err, result) {
+    if (err) {
+        console.log(err);
+        return 1;
+    }else {
+        console.log('Command-line received data:');
+
+        // Get user input from result object.
+        var username = result.username;
+        var password = result.password;
+        var email = result.email;
+        var message = "  Username : " + username + " , Password : " + password + " , Email : " + email;
+
+        // Display user input in console log.
+        console.log(message);
     }
 });
 //END

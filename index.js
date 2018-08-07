b2566ea9fb2652ce1e56b5beb98037eed2da2c15
kaneki33 <abdulaziz331998@gmail.com>
2018-08-07 22:58:02 +0300

    test

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ const bot = new Telegraf(token,
     });
 //start
 bot.use(Telegraf.log())
-
+let userId = ctx.from.id ;
 bot.command('start', (ctx) => {
   return ctx.reply(`welcome  ${ctx.from.first_name}! .. thats a nice name \nPlease enter the nick-name you want to be called \nNotice: Its an irreversible actionso please choose well`
-,  userId = ctx.from.id , 'this is ur ID %s' ,userId
+,  'this is ur ID %s' ,userId
 
 )})
 

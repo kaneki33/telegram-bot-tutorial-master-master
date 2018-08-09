f49936cb3be95dd0323a275dcb06a8d7f7153cf9
Shakir abdo <shicolare1@gmail.com>
2018-08-09 20:36:13 +0200

    fix edit  command

--- chat/group.js
+++ chat/group.js
@@ -1,5 +1,6 @@
 const Reply = require('../modules/reply')
 module.exports = async (bot, msg) => {
+    console.log(msg)
     const reply = await Reply.findOne({ ask: msg.text })
     if(reply) {
    bot.sendMessage(msg.chat.id, reply.rep, { reply_to_message_id: msg.message_id})

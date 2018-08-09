bec6a5965b5adbf65804cfa2ca88ed5c6e40a52d
fofa <you@example.comabdulaziz331998@gmail.com>
2018-08-10 02:28:37 +0300

    editing

--- chat/group.js
+++ chat/group.js
@@ -1,19 +1,29 @@
 const Reply = require('../modules/reply')
+const User  = require('../modules/user')
 module.exports = async (bot, msg) => {
     const reply = await Reply.findOne({ ask: msg.text })
-    const user = await User.findOne({id}).catch(err => false)
     if(reply) {
         if(reply.ask == "حجة" && msg.from.id == "383063938") {
 
             bot.sendMessage(msg.chat.id,`عيون الحجة 😍 `, {
                 reply_to_message_id: msg.message_id
               })            }
-              
-                  else if(reply.ask == "مين انا يا حجة؟" && msg.from.id == "383063938") {
+              else if(reply.ask == "مين انا يا حجة؟" && msg.from.id == "383063938") 
+                {
+                  
+                                const user = await User.findOne({id}).catch(err => false)
+                    if (!user) {
+                        bot.sendMessage(msg.chat.id, `مدري عنك والله`), {
+                            reply_to_message_id: msg.message_id
+                          } 
+                    } else {
+                        bot.sendMessage(msg.chat.id, `انت ${user.nickName} .. وكمان عزووووز الطيب 😍😍`), {
+                            reply_to_message_id: msg.message_id
+                          } 
+                    }
+                  
 
-                    bot.sendMessage(msg.chat.id,`انت ${user.nickName} .. وكمان عزووووز الطيب 😍😍`, {
-                        reply_to_message_id: msg.message_id
-                      })            }
+                }
             else {
                 bot.sendMessage(msg.chat.id, reply.rep)
 

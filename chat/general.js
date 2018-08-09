c3c300e162926a5464aca252da06370cc1ddc662
Shakir abdo <shicolare1@gmail.com>
2018-08-09 20:23:08 +0200

    fix edit  command

--- chat/general.js
+++ chat/general.js
@@ -3,15 +3,15 @@ module.exports = (bot, msg) => {
 switch (true) {
     case msg.text == 'معلوماتي' :
     bot.sendMessage(msg.chat.id, ` Name :  ${msg.from.first_name} \n\nID :${msg.from.id}`)
-        break
+    break
     case msg.text.startsWith('خالة قولي'):
     const match = msg.text.match(/خالة قولي (.+)/)
     bot.sendMessage(msg.chat.id,  match[1])
     break
+    case (msg.from.id == '280942102' || msg.from.id == '383063938'):
+    admin(bot, msg)
+    break
     default:
-        break
-        case (msg.from.id == '280942102' || msg.from.id == '383063938'):
-      admin(bot, msg)
     break
 }
 }

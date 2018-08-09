36f35c86735a6e2df7e2192101d2b20cbc62d3d1
fofa <you@example.comabdulaziz331998@gmail.com>
2018-08-09 17:04:24 +0300

    message here

--- bot/private.js
+++ bot/private.js
@@ -12,7 +12,7 @@ module.exports = (bot, msg) => {
 لقبي فلان
       `)
           } else {
-            bot.sendMessage(msg.chat.id, `مرحبا بك  ${user.nickName}`).then((msg) => {})
+            bot.sendMessage(msg.chat.id, ` Name :  ${msg.from.first_name} \n\nID :${msg.from.id}`).then((msg) => {})
           }
         }).catch((err) => {
           console.log(err)

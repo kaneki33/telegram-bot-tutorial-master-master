983fcdb3dfecea92ca7f3bf201369bae3932afac
fofa <you@example.comabdulaziz331998@gmail.com>
2018-08-10 00:05:04 +0300

    editing

--- chat/group.js
+++ chat/group.js
@@ -2,6 +2,13 @@ const Reply = require('../modules/reply')
 module.exports = async (bot, msg) => {
     const reply = await Reply.findOne({ ask: msg.text })
     if(reply) {
-   bot.sendMessage(msg.chat.id, reply.rep, { reply_to_message_id: msg.message_id})
+        if(reply.ask == "حجة" && msg.from.id == "383063938") {
+            bot.sendMessage(msg.chat.id , "ي عيون حجة" )
+            }else {
+                bot.sendMessage(msg.chat.id, reply.rep, { reply_to_message_id: msg.message_id})
+
+            
+            }
     }
+
 }

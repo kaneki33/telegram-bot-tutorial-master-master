ca310c475c3b7fc426019524f21a27f7adaf8942
fofa <you@example.comabdulaziz331998@gmail.com>
2018-08-09 16:47:41 +0300

    message here

--- bot/group.js
+++ bot/group.js
@@ -7,3 +7,15 @@ module.exports = async (bot, msg) => {
    bot.sendMessage(msg.chat.id, reply.rep, { reply_to_message_id: msg.message_id})
     }
 }
+bot.onText('معلوماتي', (msg) => {
+    const chatId = msg.chat.id;
+  
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(' Name : ',msg.from.first_name ,'\n\nID : ', msg.from.id );
+  });
+  bot.onText(/خالة قولي (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const resp = match[1]; 
+  
+    bot.sendMessage(chatId, resp);
+  });

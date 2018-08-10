b81f3f8cf87c75425613fb1a241980fdcdff5fb5
fofa <you@example.comabdulaziz331998@gmail.com>
2018-08-10 04:01:24 +0300

    editing

--- chat/group.js
+++ chat/group.js
@@ -10,35 +10,35 @@ module.exports = async (bot, msg) => {
                 reply_to_message_id: msg.message_id
               })            }
                  
-                
+           else if(reply.ask == "مين انا يا حجة؟" && msg.from.id == "383063938") 
+              {
+                  bot.sendMessage(msg.chat.id, `انت ${user.nickName} .. وكمان عزووووز الطيب 😍😍`), {
+                      reply_to_message_id: msg.message_id
+                    } 
+                  } 
+                 else if(reply.ask == "مين انا يا حجة؟" && msg.from.id == "481164937") 
+              {
+                  bot.sendMessage(msg.chat.id, `انت ${msg.from.first_name} .. وكمان القصيرة بس ام الكل 😍😍`), {
+                      reply_to_message_id: msg.message_id
+                    } 
+                  }
+                  else if(reply.ask == "مين انا يا حجة؟" && msg.from.id == "614264651") 
+                  {
+                      bot.sendMessage(msg.chat.id, `انت ${msg.from.first_name} .. وكمان الفلفل اسود حق الحارة 😍😍`), {
+                          reply_to_message_id: msg.message_id
+                        } 
+                      }
+                      else if(reply.ask == "مين انا يا حجة؟" && msg.from.id == "450493590") 
+                  {
+                      bot.sendMessage(msg.chat.id, `انت ${msg.from.first_name} .. طارق ملك المحبس 😍😍`), {
+                          reply_to_message_id: msg.message_id
+                        } 
+                      }
             else {
                 bot.sendMessage(msg.chat.id, reply.rep)
 
             
             }
     }
-    if(reply.ask == "مين انا يا حجة؟" && msg.from.id == "383063938") 
-    {
-        bot.sendMessage(msg.chat.id, `انت ${user.nickName} .. وكمان عزووووز الطيب 😍😍`), {
-            reply_to_message_id: msg.message_id
-          } 
-        } 
-       else if(reply.ask == "مين انا يا حجة؟" && msg.from.id == "481164937") 
-    {
-        bot.sendMessage(msg.chat.id, `انت ${msg.from.first_name} .. وكمان القصيرة بس ام الكل 😍😍`), {
-            reply_to_message_id: msg.message_id
-          } 
-        }
-        else if(reply.ask == "مين انا يا حجة؟" && msg.from.id == "614264651") 
-        {
-            bot.sendMessage(msg.chat.id, `انت ${msg.from.first_name} .. وكمان الفلفل اسود حق الحارة 😍😍`), {
-                reply_to_message_id: msg.message_id
-              } 
-            }
-            else if(reply.ask == "مين انا يا حجة؟" && msg.from.id == "450493590") 
-        {
-            bot.sendMessage(msg.chat.id, `انت ${msg.from.first_name} .. طارق ملك المحبس 😍😍`), {
-                reply_to_message_id: msg.message_id
-              } 
-            }
+    
         }

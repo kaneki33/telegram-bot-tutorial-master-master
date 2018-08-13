0b95c3ae6d80031feb4fbce2089b7bba77995908
fofa <you@example.comabdulaziz331998@gmail.com>
2018-08-13 03:58:19 +0300

    editing

--- chat/general.js
+++ chat/general.js
@@ -1,8 +1,18 @@
 const admin   = require('./admin')
 const news    = require('../features/news')
+const news    = require('../features/lyrics')
+
 module.exports = (bot, msg) => {
     const text = String(msg.text) || ""
 switch (true) {
+    case text.startsWith('Lyric'):
+    const matches = text.match(/Lyric(\s+)(.+)(\s+)-(\s+)(.+)/)
+    new Reply({
+    art: matches[2],
+    song: matches[5]
+    })
+    lyric(art,song)
+        break 
     case text == 'معلوماتي' :
     bot.sendMessage(msg.chat.id, ` Name :  ${msg.from.first_name} \n\nID :${msg.from.id}`)
     news(bot, msg)

60673705231a51659b2b7558ef8e8f5caac8d313
kaneki33 <abdulaziz331998@gmail.com>
2018-10-03 16:03:45 +0300

    adding urban dictionary

--- chat/general.js
+++ chat/general.js
@@ -2,12 +2,90 @@ const admin   = require('./admin')
 const news    = require('../features/news')
 const lyric    = require('../features/lyrics')
 const dict    = require('../features/dict')
-
+//const dict    = require('../features/urban')
+const ud = require('../urban-dictionary')
 module.exports = (bot, msg) => {
     const text = String(msg.text) || ""
-switch (true) {
-    case text.startsWith('tube'):
+switch (true) {  
+    case text.startsWith('/ud'):
+var definition = 'car'
+var id = 217456
+
+// defid callback example.
+ud.defid(id, (error, entry) => {
+  if (error) {
+    console.error(error.message)
+  } else {
+    console.log(entry.word)
+    console.log(entry.definition)
+    console.log(entry.example)
+  }
+})
+// defid promise example.
+ud.defid(id).then((result) => {
+  console.log(result.word)
+  console.log(result.definition)
+  console.log(result.example)
+  bot.sendMessage(msg.chat.id, entry.word)
+}).catch((error) => {
+  console.error(error.message)
+})
+
+// random callback example.
+ud.random((error, entry) => {
+  if (error) {
+    console.error(error.message)
+  } else {
+    console.log(entry.word)
+    console.log(entry.definition)
+    console.log(entry.example)
+    bot.sendMessage(msg.chat.id, 'random call')
+
+  }
+})
+
+// random promise example.
+ud.random().then((result) => {
+  console.log(result.word)
+  console.log(result.definition)
+  console.log(result.example)
+  bot.sendMessage(msg.chat.id, 'ran pro')
+
+}).catch((error) => {
+  console.error(error.message)
+})
+
+// term callback example.
+ud.term(definition, (error, entries, tags, sounds) => {
+  if (error) {
+    console.error(error.message)
+  } else {
+    console.log(entries[0].word)
+    console.log(entries[0].definition)
+    console.log(entries[0].example)
+    bot.sendMessage(msg.chat.id, 'term call')
+
+  }
+})
+
+// term promise example.
+ud.term(definition).then((result) => {
+  const entries = result.entries
+  console.log(entries[0].word)
+  console.log(entries[0].definition)
+  console.log(entries[0].example)
+  bot.sendMessage(msg.chat.id, 'term pro')
+
+}).catch((error) => {
+  console.error(error.message)
+})
+
     
+
+
+
+
+        break 
     case text.startsWith('Lyric'):
     const matches = text.match(/Lyric(\s+)(.+)(\s+)-(\s+)(.+)/)
     lyric.get(matches[2],matches[5], (err, lyrics) => {

d57e26c9f0f05d18d6b8f368e2f2d4135833ac87
kaneki33 <abdulaziz331998@gmail.com>
2018-10-03 16:16:41 +0300

    editing urban dictionary

--- chat/general.js
+++ chat/general.js
@@ -16,17 +16,17 @@ ud.defid(id, (error, entry) => {
   if (error) {
     console.error(error.message)
   } else {
-    console.log(entry.word)
-    console.log(entry.definition)
-    console.log(entry.example)
+    bot.sendMessage(msg.chat.id,entry.word)
+    bot.sendMessage(msg.chat.id,entry.definition)
+    bot.sendMessage(msg.chat.id,entry.example)
   }
 })
 // defid promise example.
 ud.defid(id).then((result) => {
-  console.log(result.word)
-  console.log(result.definition)
-  console.log(result.example)
-  bot.sendMessage(msg.chat.id, entry.word)
+  bot.sendMessage(msg.chat.id,result.word)
+  bot.sendMessage(msg.chat.id,result.definition)
+  bot.sendMessage(msg.chat.id,result.example)
+  bot.sendMessage(msg.chat.id, 'defid pro')
 }).catch((error) => {
   console.error(error.message)
 })
@@ -36,9 +36,9 @@ ud.random((error, entry) => {
   if (error) {
     console.error(error.message)
   } else {
-    console.log(entry.word)
-    console.log(entry.definition)
-    console.log(entry.example)
+    bot.sendMessage(msg.chat.id,entry.word)
+    bot.sendMessage(msg.chat.id,entry.definition)
+    bot.sendMessage(msg.chat.id,entry.example)
     bot.sendMessage(msg.chat.id, 'random call')
 
   }
@@ -46,9 +46,9 @@ ud.random((error, entry) => {
 
 // random promise example.
 ud.random().then((result) => {
-  console.log(result.word)
-  console.log(result.definition)
-  console.log(result.example)
+  bot.sendMessage(msg.chat.id,result.word)
+  bot.sendMessage(msg.chat.id,result.definition)
+  bot.sendMessage(msg.chat.id,result.example)
   bot.sendMessage(msg.chat.id, 'ran pro')
 
 }).catch((error) => {
@@ -60,9 +60,9 @@ ud.term(definition, (error, entries, tags, sounds) => {
   if (error) {
     console.error(error.message)
   } else {
-    console.log(entries[0].word)
-    console.log(entries[0].definition)
-    console.log(entries[0].example)
+    bot.sendMessage(msg.chat.id,entries[0].word)
+    bot.sendMessage(msg.chat.id,entries[0].definition)
+    bot.sendMessage(msg.chat.id,entries[0].example)
     bot.sendMessage(msg.chat.id, 'term call')
 
   }
@@ -71,9 +71,9 @@ ud.term(definition, (error, entries, tags, sounds) => {
 // term promise example.
 ud.term(definition).then((result) => {
   const entries = result.entries
-  console.log(entries[0].word)
-  console.log(entries[0].definition)
-  console.log(entries[0].example)
+  bot.sendMessage(msg.chat.id,entries[0].word)
+  bot.sendMessage(msg.chat.id,entries[0].definition)
+  bot.sendMessage(msg.chat.id,entries[0].example)
   bot.sendMessage(msg.chat.id, 'term pro')
 
 }).catch((error) => {

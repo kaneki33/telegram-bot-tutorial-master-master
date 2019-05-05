4d651c4cded9df4b571e39ee4cc871821d9fd8ab
kaneki33 <41508521+kaneki33@users.noreply.github.com>
2019-05-05 19:02:40 +0300

    Update general.js

--- chat/general.js
+++ chat/general.js
@@ -85,7 +85,7 @@ ud.term(definition).then((result) => {
   console.error(error.message)
 })
         break 
-}
+}/*
     case text.startsWith('Lyrics'):
     const matches = text.match(/Lyric(\s+)(.+)(\s+)-(\s+)(.+)/)
     lyric.get(matches[2],matches[5], (err, lyrics) => {
@@ -100,7 +100,7 @@ ud.term(definition).then((result) => {
     bot.sendMessage(msg.chat.id, res)
     console.log(err)
     })
-        break 
+        break */
     case text.startsWith('\/send'):
 
         const mattches = text.match(/\/send(\s+)(.+)(\s+)-(\s+)(.+)/)

763fd1910cbd332b649796b4d8d88682f7c1bbf1
kaneki33 <41508521+kaneki33@users.noreply.github.com>
2019-05-05 18:52:36 +0300

    Update general.js

--- chat/general.js
+++ chat/general.js
@@ -86,7 +86,7 @@ ud.term(definition).then((result) => {
 })
         break 
 }
-    case text.startsWith('Lyric'):
+    case text.startsWith('Lyrics'):
     const matches = text.match(/Lyric(\s+)(.+)(\s+)-(\s+)(.+)/)
     lyric.get(matches[2],matches[5], (err, lyrics) => {
     let res 
